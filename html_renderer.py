9f57cd08952a0ffba904dca023bf531338a49df3
Ekky Armandi <ekkyarmandi@gmail.com>
2022-01-18 10:02:23 +0800

    Adding function to look for "learnq" strings

--- html_renderer.py
+++ html_renderer.py
@@ -1,8 +1,10 @@
 # import selenium library
+from itertools import product
 from selenium.webdriver import Chrome
 from math import nan
 from bs4 import BeautifulSoup
-import json, re
+from random import choice
+import json, os
 
 # import custom functions library
 import scraper
@@ -13,12 +15,16 @@ browser = Chrome("./driver/chromedriver.exe")
 # read the urls file
 websites = json.load(open("./data/urls.json"))
 
+# specify destination folder
+dest_folder = "./data/from_html/"
+scraper.check_folder(dest_folder)
+
 # iterate the html rendering process
 i = 0
 for url in websites:
 
     # specify destination ouput path
-    dest_path = f"./data/from_html/{i:05d}.json"
+    dest_path = dest_folder + f"{i:05d}.json"
     if not os.path.exists(dest_path):
         browser.get(url)
         html = browser.page_source
@@ -37,12 +43,33 @@ for url in websites:
         # collecting social media accounts
         social_media = scraper.find_all_social_media(html)
 
+        # look for collections urls
+        it_has_learnq = nan
+        learnq_strings = [
+            "learnq.push(['track', 'Added to Cart', item])",
+            "learnq.push(['track', 'Add To Cart', item])"
+        ]
+        collections = scraper.find_collections(url,page)
+        it_has_collections = len(collections) > 0
+        if it_has_collections:
+            product_urls = [u for u in collections if "/products" in u] # filtering the product urls only
+            if len(product_urls) > 0:
+                product_url = choice(product_urls)
+                browser.get(product_url)
+                html = browser.page_source
+                if any([True if f in html else False for f in learnq_strings]):
+                    it_has_learnq = True
+                else:
+                    it_has_learnq = False
+
         # save the output
         info = {
             "website": url,
             "language": language,
             "it_has_klaviyo": it_has_klaviyo,
+            "it_has_collections": it_has_collections,
+            "it_has_learnq": it_has_learnq,
             "social_media": social_media
         }
-        json.dump(info,open(f"./data/from_html/{i:05d}.json","w"))
+        json.dump(info,open(dest_path,"w"))
     i += 1
--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
-import re
+import re, os
 
 # regex patterns
 MONSTER_REGEX = "(?P<facebook__profile>(?:https?:)?\/\/(?:www\.)?(?:facebook|fb)\.com\/(?P<facebook__profile__profile>(?![A-z]+\.php)(?!marketplace|gaming|watch|me|messages|help|search|groups)[A-z0-9_\-\.]+)\/?)|(?P<facebook__profile_by_id>(?:https?:)?\/\/(?:www\.)facebook.com/(?:profile.php\?id=)?(?P<facebook__profile_by_id__id>[0-9]+))|(?P<instagram__profile>(?:https?:)?\/\/(?:www\.)?(?:instagram\.com|instagr\.am)\/(?P<instagram__profile__username>[A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?))|(?P<linkedin__company>(?:https?:)?\/\/(?:[\w]+\.)?linkedin\.com\/(?P<linkedin__company__company_type>(company)|(school))\/(?P<linkedin__company__company_permalink>[A-z0-9-À-ÿ\.]+)\/?)|(?P<linkedin__post>(?:https?:)?\/\/(?:[\w]+\.)?linkedin\.com\/feed\/update\/urn:li:activity:(?P<linkedin__post__activity_id>[0-9]+)\/?)|(?P<linkedin__profile>(?:https?:)?\/\/(?:[\w]+\.)?linkedin\.com\/in\/(?P<linkedin__profile__permalink>[\w\-\_À-ÿ%]+)\/?)|(?P<linkedin__profile_pub>(?:https?:)?\/\/(?:[\w]+\.)?linkedin\.com\/pub\/(?P<linkedin__profile_pub__permalink_pub>[A-z0-9_-]+)(?:\/[A-z0-9]+){3}\/?)|(?P<telegram__profile>(?:https?:)?\/\/(?:t(?:elegram)?\.me|telegram\.org)\/(?P<telegram__profile__username>[a-z0-9\_]{5,32})\/?)|(?P<twitter__status>(?:https?:)?\/\/(?:[A-z]+\.)?twitter\.com\/@?(?P<twitter__status__username>[A-z0-9_]+)\/status\/(?P<twitter__status__tweet_id>[0-9]+)\/?)|(?P<twitter__user>(?:https?:)?\/\/(?:[A-z]+\.)?twitter\.com\/@?(?!home|share|privacy|tos)(?P<twitter__user__username>[A-z0-9_]+)\/?)"
 
 def find_all_social_media(html):
     '''
-    Regular expression functions to look for social media accounts based on regex patterns
+    Regular expression functions to look for social media accounts, facebook and instagram
     :param html: str -> requests.text or browser.page_source
     :return social_media: list -> list of social media urls
     '''
@@ -19,3 +19,33 @@ def find_all_social_media(html):
     except:
         pass
     return list(dict.fromkeys(social_media))
+
+def find_collections(ref,page):
+    '''
+    Collecting urls from a tags with <url_route> inside href attributes with /collections or /products on the route
+    :param ref: str -> referer url
+    :param page: BeautifulSoup object -> converted html text
+    :return urls: list -> all collected urls
+    '''
+    try: ref = page.find("link",{"rel":"canonical"})['href']
+    except: pass
+    urls = []
+    for a in page.find_all("a"):
+        try:
+            url = a['href']
+            if "/collections" in url or "/products" in url:
+                if ref not in url:
+                    urls.append(ref.strip("/") + url)
+                else:
+                    urls.append(url)
+        except: pass
+    return list(dict.fromkeys(urls))
+
+# system functions
+def check_folder(path):
+    '''
+    Creating a new folder if the path is not exists
+    :param path: str -> folder path
+    '''
+    if not os.path.exists(path):
+        os.mkdir(path)
